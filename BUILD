d2c03204064c48f2d0051907a1ed6e83a60b68ea
wing-anemone <utidaaya@163.com>
2023-06-17 00:27:35 +0800

    init bazel & cmake

---
+++ BUILD
@@ -0,0 +1,12 @@
+
+
+cc_binary(
+  name = "main",
+  srcs = glob([
+        "src/**/*.cpp",
+        "src/**/*.h",
+    ]),
+  copts = [
+    "-std=c++17"
+  ],
+)
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+std::vector<int> vec{1,2,3,4};
+int main(){
+  cout << "ka a i" << endl;
+  return 0;
+}
